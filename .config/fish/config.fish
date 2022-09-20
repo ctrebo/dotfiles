a6dc9dbda7608924e1e1ab9f5eecf9c9e1aa5983
Christoffer Trebo <christoffer.trebo@gmail.com>
2022-09-20 16:55:37 +0200

    add vi mode to terminal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,10 @@
 set fish_greeting "" 
 
-oh-my-posh init fish --config /mnt/c/Users/chris/poshthemes/takuya.omp.json | source
+#oh-my-posh init fish --config /mnt/c/Users/chris/poshthemes/takuya.omp.json | source
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    eval /home/chris/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
 
 
@@ -12,9 +13,14 @@ alias ls="exa --icons"
 alias cat="batcat"
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias vim="nvim"
+alias fd="fdfind"
+
+# Vim keytypes
+fish_vi_key_bindings
+bind -M insert -m default jk backward-char force-repaint
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/chris/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+#eval /home/chris/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
